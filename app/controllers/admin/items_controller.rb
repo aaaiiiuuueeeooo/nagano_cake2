cdaed620932010af93e91afd8d4ef3bd021d249c
EC2 Default User <107116870+aaaiiiuuueeeooo@users.noreply.github.com>
2022-08-21 10:49:46 +0000

    [nagano_cake7]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,26 +1,32 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def index
     @item = Item.new
     @items = Item.all
-  end 
-  
+  end
+
   def new
     @item = Item.new
-  end 
-  
+  end
+
   def create
     @item = Item.new(item_params)
     @item.save
     redirect_to admin_items_path
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:introduction,:genre_id,:price,:is_active)
   end
 end
