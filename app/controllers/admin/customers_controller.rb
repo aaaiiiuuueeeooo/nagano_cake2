0dafdc8a6c75fa66ddbb9dcbbdf6ebf1fd9dae1f
EC2 Default User <107116870+aaaiiiuuueeeooo@users.noreply.github.com>
2022-08-28 13:54:04 +0000

    [nagano_cake12]

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,4 +3,12 @@ class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.all 
   end 
+  
+  def show
+    @customers = Customer.all
+    @customer = Customer.find(params[:id])
+  end 
+  
+  def update
+  end 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :genre
+  has_one_attached :image
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/noimage.png')
+      image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
+    end
+    image
+  end
 end
