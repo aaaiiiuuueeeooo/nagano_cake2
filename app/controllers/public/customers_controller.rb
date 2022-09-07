4c5ee06a863ff7c097f97b0399f955492db580a5
EC2 Default User <107116870+aaaiiiuuueeeooo@users.noreply.github.com>
2022-09-07 13:27:16 +0000

    [nagano_cake18]

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,6 +1,16 @@
 class Public::CustomersController < ApplicationController
-  
+
   def show
     @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(user_params)
+    redirect_to customer_path(@customer.id)
   end
 end
